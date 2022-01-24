8aa389da1c9dd3550988f37f23447306a897539d
Rob Hess <hessro@oregonstate.edu>
2022-01-24 13:58:50 -0800

    Update menu data.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import React from 'react';
 import './App.css';
 
 const menu = {
-  tacos: "https://media.giphy.com/media/KfxPgR9Xb6lRvlFa8x/giphy.gif",
-  pizza: "https://media.giphy.com/media/VCDSo9xqCJOjC/giphy.gif",
-  sushi: "https://media1.tenor.com/images/a7087e13ce68524779c9b6946818986b/tenor.gif"
+  tacos: {
+    name: "Tacos",
+    image: "https://media.giphy.com/media/KfxPgR9Xb6lRvlFa8x/giphy.gif",
+    description: "Shell + fillings",
+    price: 5.95
+  },
+  pizza: {
+    name: "Pizza",
+    image: "https://media.giphy.com/media/VCDSo9xqCJOjC/giphy.gif",
+    description: "Crust, sauce, cheese",
+    price: 19.95
+  },
+  sushi: {
+    name: "Sushi",
+    image: "https://media1.tenor.com/images/a7087e13ce68524779c9b6946818986b/tenor.gif",
+    description: "Raw fish + rice",
+    price: 10.95
+  }
 };
 
 function Home() {
